42fa91a6769213b30065a3f0e2e0f8dc9f019390
Juan Tenezaca <tenj34@gmail.com>
2018-07-07 18:14:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,12 @@
 def badge_creator(names)
   badges = []
   names.each do |name|
-    badges. << badge_maker(name)
+    badges << badge_maker(name)
   end
   badges
 end
 
 def badge_maker(name)
-  
+
 
 end
